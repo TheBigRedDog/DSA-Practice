24d029db2579d60a449bf6996b2e2a63ea91a706
TheBigRedDog <clmmoran@ucdavis.edu>
2022-06-22 08:45:19 -0700

    implemented LC-875 'koko eating bananas' in python

---
+++ LC-875-koko_eating_bananas-binary_search/koko_eating_banans.py
@@ -0,0 +1,26 @@
+'''
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+'''
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        while left < right:
+            m = (left + right) // 2
+
+            hours_eating = 0
+            for pile in piles:
+                hours_eating += math.ceil(pile/m)
+            if hours_eating <= h:
+                right = m
+            else:
+                left = m + 1
+            
+        return right
