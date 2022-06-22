c029bfe47ecdc4745afd880bdc367adf7303650c
TheBigRedDog <clmmoran@ucdavis.edu>
2022-06-22 10:27:43 -0700

    implemented LC-33 'search in rotated array' with javascript

--- LC-33-search_in_rotated_array-binary_search/search_in_rotated_array.js
+++ LC-33-search_in_rotated_array-binary_search/search_in_rotated_array.js
@@ -1,4 +1,4 @@
-'''
+/*
 There is an integer array nums sorted in ascending order (with distinct values).
 
 Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
@@ -6,8 +6,9 @@ Prior to being passed to your function, nums is possibly rotated at an unknown p
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
 You must write an algorithm with O(log n) runtime complexity.
-'''
-'''
+*/
+
+/*
 Solution explanation:
 When sorted array is shifted => after taking the middle, one side will always be sorted
 1. take middle and compare with target. If match, return middle
@@ -17,28 +18,35 @@ When sorted array is shifted => after taking the middle, one side will always be
 3. If middle < right side, right side is sorted
 3a. Check if target in right side with middle < target <= right. If target in there, recurse on that side.
 3b. Target not in right side, recuse on left side
-'''
+*/
 
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        
-        l, r = 0, len(nums)-1
-        
-        while l <= r:
-            m = l + (r-l)//2
-            
-            if nums[m] == target:
-                return m
-            
-            elif nums[m] >= nums[l]:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
-            elif nums[m] <= nums[r]:
-                if nums[m] < target <= nums[r]:
-                    l = m + 1
-                else:
-                    r = m - 1
-        return -1
-                    
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+    if (nums[m] == target) {
+      return m;
+    }
+    if (nums[m] >= nums[l]) {
+      if (nums[l] <= target && target < nums[m]) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
+    } else {
+      if (nums[m] < target && target <= nums[r]) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+  }
+  return -1;
+};
