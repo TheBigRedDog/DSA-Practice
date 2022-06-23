1f01946e340528dadc1e6f1a165edac504887645
TheBigRedDog <clmmoran@ucdavis.edu>
2022-06-22 17:04:13 -0700

    implemented LC-153 'find minimum in rotated sorted array' with python

---
+++ LC-153-find_minimum_in_rotated_sorted_array-binary_search/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+'''
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+'''
+class Solution:
+    def findMin(self, nums: list) -> int:
+        r = len(nums)-1
+        l = 0
+        
+        while l < r:
+            m = l + (r-l)//2
+            
+            if nums[m-1] > nums[m] < nums[m+1]:
+                return nums[m]
+            if nums[m] > nums[-1]:
+                l = m + 1
+            elif nums[m] < nums[-1]:
+                r = m - 1
+            
+        
+        return nums[l]
